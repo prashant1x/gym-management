c4a34acb4fb0d9c1c3e45832eaa3590f2400ac20
unknown <jadhavprashant713@gmail.com>
2018-03-03 19:33:21 +0530

    added changes for testing

--- application/view/member/reps_dashboard.php
+++ application/view/member/reps_dashboard.php
@@ -50,5 +50,47 @@ if (isset($GLOBALS['RepsList']) && sizeof($GLOBALS['RepsList']) > 0) {
     echo "<center>No reps found.</center>";
 }
 ?>
+<center><div class="chart-container" style="position: relative; height:50%; width:80% ">
+<canvas id="bar-chart"></canvas>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script>
+function dashedBorder(chart, dataset, data, dash) {
+
+// edit the .draw() function
+chart.config.data.datasets[dataset]._meta[0].data[data].draw = function() {
+    chart.chart.ctx.setLineDash(dash);
+    Chart.elements.Rectangle.prototype.draw.apply(this, arguments);
+    Chart.defaults.global.elements.rectangle.borderWidth = 2;
+
+    // put the line style back to the default value
+    chart.chart.ctx.setLineDash([1,0]);
+}
+}
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+        
+      legend: { display: false },
+      title: {
+        display: true,
+        drawonchartarea: true,
+        
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+</script>
+</center></div>
     </body>
 </html>
